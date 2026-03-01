46dbf1890552c64cd632af11e9ca8d18bd122966
Agus <aguscm23@gmail.com>
2026-03-01 11:12:22 +0100

    feat: update build script to copy images after build and fix target directory path

--- scripts/copy-images.js
+++ scripts/copy-images.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const sourceDir = path.join(__dirname, "..", "src", "modules", "products", "data", "images");
-const targetDir = path.join(__dirname, "..", "built", "modules", "products", "data", "images");
+const targetDir = path.join(__dirname, "..", "build", "modules", "products", "data", "images");
 
 if (!fs.existsSync(sourceDir)) {
   console.warn(`[postbuild] Source folder not found: ${sourceDir}`);
